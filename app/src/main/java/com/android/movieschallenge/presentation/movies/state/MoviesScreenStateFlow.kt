3402b1d3b0647d8fbf1f03f9f4d95dd5d3a27533
Ivan Aliaga <ivanaliaga22@gmail.com>
2024-02-18 00:31:14 -0500

    se actualizó a collectAsStateWithLifecycle en combinedState

--- app/src/main/java/com/android/movieschallenge/presentation/movies/state/MoviesScreenStateFlow.kt
+++ app/src/main/java/com/android/movieschallenge/presentation/movies/state/MoviesScreenStateFlow.kt
@@ -2,11 +2,12 @@ package com.android.movieschallenge.presentation.movies.state
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.android.movieschallenge.domain.model.Movie
 import com.android.movieschallenge.presentation.LoaderFullScreen
 import kotlinx.collections.immutable.toImmutableList
@@ -14,8 +15,10 @@ import kotlinx.collections.immutable.toImmutableList
 @Composable
 fun MoviesScreenStateFlow(toMovieDetail: (Int) -> Unit) {
     val viewModel: MoviesStateFlowMovieModel = hiltViewModel()
-    val combinedState by viewModel.combinedState
-        .collectAsState(initial = Pair(emptyList<Movie>().toImmutableList(), false))
+    val combinedState by viewModel.combinedState.collectAsStateWithLifecycle(
+        initialValue = Pair(emptyList<Movie>().toImmutableList(), false),
+        lifecycleOwner = LocalLifecycleOwner.current
+    )
 
     val movies = combinedState.first
     val isLoading = combinedState.second
