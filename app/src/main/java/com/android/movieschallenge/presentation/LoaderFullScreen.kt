86f13920beb657b4887bb04270880db5e6dd1b7f
Ivan Aliaga <ivanaliaga22@gmail.com>
2024-02-18 23:39:13 -0500

    El loader está animado con una expansión desde el centro hasta lo máximo del ancho de la pantalla

--- app/src/main/java/com/android/movieschallenge/presentation/LoaderFullScreen.kt
+++ app/src/main/java/com/android/movieschallenge/presentation/LoaderFullScreen.kt
@@ -1,26 +1,56 @@
 package com.android.movieschallenge.presentation
 
-import android.annotation.SuppressLint
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.dp
 
-@SuppressLint("CoroutineCreationDuringComposition")
 @Composable
 fun LoaderFullScreen() {
+    val screenWidthDp = LocalConfiguration.current.screenWidthDp
+    var expanded by remember { mutableStateOf(false) }
+    val width = animateDpAsState(
+        targetValue = if (expanded) screenWidthDp.dp else 50.dp,
+        animationSpec = tween(durationMillis = 1000), label = "animateDpAsStateWidth"
+    )
+
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Black.copy(alpha = 0.5f)),
+            .background(Color.Transparent),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        CircularProgressIndicator()
+        val color = Color.Black.copy(alpha = 0.5f)
+        Box(
+            modifier = Modifier
+                .size(width.value, width.value)
+                .background(color = color,
+                    shape = CircleShape),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator()
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        expanded = true
     }
 }
